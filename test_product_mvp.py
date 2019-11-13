b867c81950795b06e5ddbc611c8232a4bcc415b4
jslodkowicz <jan.slod@gmail.com>
2019-11-13 09:39:35 +0100

    tests refactor to pytest, added pre-commit

--- test_product_mvp.py
+++ test_product_mvp.py
@@ -1,24 +1,24 @@
 from app.product_mvp import percentage, price_calc
-import unittest
 
 
-class TestProduct(unittest.TestCase):
+class TestProduct:
     def test_percentage(self):
-        self.assertEqual(percentage(100, 1), 1)
-        self.assertEqual(percentage(5000, 20), 1000)
+        assert percentage(100, 1) == 1
+        assert percentage(5000, 20) == 1000
+        assert percentage(10, 5) == 0.5
 
     def test_price_calc_no_discount(self):
-        result = price_calc(10, 10, "UT")
-        self.assertEqual(result, "Total price will be 106.85 USD")
+        res_1 = price_calc(10, 10, "UT")
+        res_2 = price_calc(30, 15, "TX")
+        res_3 = price_calc(200, 3.99, "NV")
+        assert res_1 == "Total price will be 106.85 USD"
+        assert res_2 == "Total price will be 478.12 USD"
+        assert res_3 == "Total price will be 861.84 USD"
 
     def test_price_calc_with_discount(self):
-        result = price_calc(7, 1000, "CA")
-        self.assertEqual(result, "Total price will be 7047.07 USD")
-
-    def test_price_calc_error(self):
-        price_calc(8, 30, "WRONG")
-        self.assertRaises(KeyError)
-
-
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+        res_1 = price_calc(7, 1000, "CA")
+        res_2 = price_calc(100, 20, "TX")
+        res_3 = price_calc(1000, 50, "AL")
+        assert res_1 == "Total price will be 7047.07 USD"
+        assert res_2 == "Total price will be 2061.25 USD"
+        assert res_3 == "Total price will be 44200.0 USD"
